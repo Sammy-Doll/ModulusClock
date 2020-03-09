9069fdd1708846ee42b3a65e438c554af4f06233
Sammy Doll <samuel.doll.2023@student.lhschools.org>
2020-03-09 14:15:03 -0400

    UserDataCaptured

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,26 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ModulusClock();
+    }
+
+    public static void ModulusClock() {
+
+    klepto = new Scanner(System.in);
+
+    Int hour;
+    Int minute;
+    System.out.println("Hello mortal. My constituents have highly awaited your pathetic arrival. Please, enter the current hour.");
+    hour = klepto.nextLine();
+
+    System.out.println("Wait, it's that time already?!");
+    System.out.println("*whispering*");
+    System.out.println("And, um... what be the current minutes?");
+    minute = klepto.nextLine();
+
     }
 }
