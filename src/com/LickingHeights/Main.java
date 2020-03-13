cdc018248291cee43a2461eb6936a613de39cb5b
Sammy Doll <samuel.doll.2023@student.lhschools.org>
2020-03-13 13:46:15 -0400

    safetyCommit

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,21 +6,32 @@ public class Main {
 
     public static void main(String[] args) {
         ModulusClock();
+
     }
 
     public static void ModulusClock() {
-
+    Scanner klepto;
     klepto = new Scanner(System.in);
 
-    Int hour;
-    Int minute;
+    int hour;
+    int minute;
     System.out.println("Hello mortal. My constituents have highly awaited your pathetic arrival. Please, enter the current hour.");
-    hour = klepto.nextLine();
+    hour = klepto.nextInt();
 
     System.out.println("Wait, it's that time already?!");
     System.out.println("*whispering*");
     System.out.println("And, um... what be the current minutes?");
-    minute = klepto.nextLine();
+    minute = klepto.nextInt();
+
+    int secondHour
+    int secondMinute = minute > 59 ? (minute%60)+hour:minute;
+    int thirdHour = hour > 12 ? hour%12:hour;
+    int fourthHour = thirdHour > 1 ? hour:hour+1;
+
+
+    System.out.println("So, the time is "+fourthHour+":"+ secondMinute);
+    System.out.println("... fuc-");
+
 
     }
 }
